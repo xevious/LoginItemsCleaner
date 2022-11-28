a6e89a55cfa354864ae2d66e04d3d3b6794324b6
Douglas Bergere <douglas@uta.edu>
2022-11-28 15:59:06 -0600

    moved to Utilities folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 osacompile -o Remove\ Browsers\ from\ Login\ Items.app Remove\ Browsers\ from\ Login\ Items.applescript
-rm -Rf /Applications/Remove\ Browsers\ from\ Login\ Items.app
-mv Remove\ Browsers\ from\ Login\ Items.app /Applications
+rm -Rf /Applications/Utilities/Remove\ Browsers\ from\ Login\ Items.app
+mv Remove\ Browsers\ from\ Login\ Items.app /Applications/Utilities/
 
