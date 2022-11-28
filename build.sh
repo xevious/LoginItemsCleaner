021db90d69cdb62f8b843386a25012f6b95503ca
Douglas Bergere <douglas@uta.edu>
2022-11-28 15:55:14 -0600

    build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+osacompile -o Remove\ Browsers\ from\ Login\ Items.app Remove\ Browsers\ from\ Login\ Items.applescript
+rm -Rf /Applications/Remove\ Browsers\ from\ Login\ Items.app
+mv Remove\ Browsers\ from\ Login\ Items.app /Applications
+
